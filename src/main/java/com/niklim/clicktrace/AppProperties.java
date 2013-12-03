f4a4d0930f143ad41eca68f4fb9cb6e9bb48a216
juniorasty <juniorasty@gmail.com>
2013-12-03 20:27:15 +0100

    non exisiting app.properties fixed

--- src/main/java/com/niklim/clicktrace/AppProperties.java
+++ src/main/java/com/niklim/clicktrace/AppProperties.java
@@ -1,7 +1,8 @@
 package com.niklim.clicktrace;
 
-import java.awt.Rectangle;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -12,80 +13,103 @@ import com.google.inject.Singleton;
 public class AppProperties {
 	private static final String JIRA_USERNAME = "jira.username";
 	private static final String JIRA_URL = "jira.url";
-	private static final String CAPTURE_DIM_RIGHT_BOTTOM_Y = "capture.dim.rightBottomY";
-	private static final String CAPTURE_DIM_RIGHT_BOTTOM_X = "capture.dim.rightBottomX";
-	private static final String CAPTURE_DIM_LEFT_TOP_Y = "capture.dim.leftTopY";
-	private static final String CAPTURE_DIM_LEFT_TOP_X = "capture.dim.leftTopX";
-	private static final String SESSIONS_DIR = "sessions.dir";
+	// private static final String CAPTURE_DIM_RIGHT_BOTTOM_Y =
+	// "capture.dim.rightBottomY";
+	// private static final String CAPTURE_DIM_RIGHT_BOTTOM_X =
+	// "capture.dim.rightBottomX";
+	// private static final String CAPTURE_DIM_LEFT_TOP_Y =
+	// "capture.dim.leftTopY";
+	// private static final String CAPTURE_DIM_LEFT_TOP_X =
+	// "capture.dim.leftTopX";
+	// private static final String SESSIONS_DIR = "sessions.dir";
 	private static final String CAPTURE_FREQUENCY = "capture.frequency";
 	private static final String IMAGE_EDITOR_PATH = "imageEditor.path";
 	private static final String APP_PROPERTIES_PATH = "app.properties";
 
-	private PropertiesConfiguration prop;
+	private PropertiesConfiguration props;
+
+	private static final Map<String, Object> defaults;
+	static {
+		defaults = new HashMap<String, Object>();
+		defaults.put(CAPTURE_FREQUENCY, 1.0);
+	}
 
 	public AppProperties() {
 		try {
-			prop = new PropertiesConfiguration(new File(APP_PROPERTIES_PATH));
+			props = new PropertiesConfiguration(new File(APP_PROPERTIES_PATH));
+			initDefaults();
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void initDefaults() {
+		for (String key : defaults.keySet()) {
+			Object property = props.getProperty(key);
+			if (property == null) {
+				props.setProperty(key, defaults.get(key));
+			}
+		}
+	}
+
 	public void save() {
 		try {
-			prop.save();
+			props.save();
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public String getImageEditorPath() {
-		return prop.getString(IMAGE_EDITOR_PATH);
+		return props.getString(IMAGE_EDITOR_PATH);
 	}
 
 	public void setImageEditorPath(String imageEditorPath) {
-		prop.setProperty(IMAGE_EDITOR_PATH, imageEditorPath);
+		props.setProperty(IMAGE_EDITOR_PATH, imageEditorPath);
 	}
 
 	public double getCaptureFrequency() {
-		return prop.getDouble(CAPTURE_FREQUENCY);
+		return props.getDouble(CAPTURE_FREQUENCY);
 	}
 
 	public void setCaptureFrequency(double captureFrequency) {
-		prop.setProperty(CAPTURE_FREQUENCY, captureFrequency);
+		props.setProperty(CAPTURE_FREQUENCY, captureFrequency);
 	}
 
-	public String getSessionsDir() {
-		return prop.getString(SESSIONS_DIR);
-	}
+	// public String getSessionsDir() {
+	// return prop.getString(SESSIONS_DIR);
+	// }
+	//
+	// public void setSessionsDir(String sessionsDir) {
+	// prop.setProperty(SESSIONS_DIR, sessionsDir);
+	// }
 
-	public void setSessionsDir(String sessionsDir) {
-		prop.setProperty(SESSIONS_DIR, sessionsDir);
-	}
-
-	public Rectangle getCaptureRectangle() {
-		int leftTopX = prop.getInt(CAPTURE_DIM_LEFT_TOP_X);
-		int leftTopY = prop.getInt(CAPTURE_DIM_LEFT_TOP_Y);
-		int rightBottomX = prop.getInt(CAPTURE_DIM_RIGHT_BOTTOM_X);
-		int rightBottomY = prop.getInt(CAPTURE_DIM_RIGHT_BOTTOM_Y);
-
-		return new Rectangle(leftTopX, leftTopY, rightBottomX - leftTopX, rightBottomY - leftTopY);
-	}
-
-	public void setCaptureRectangle(Rectangle rect) {
-		prop.setProperty(CAPTURE_DIM_LEFT_TOP_X, String.valueOf(rect.getMinX()));
-		prop.setProperty(CAPTURE_DIM_LEFT_TOP_Y, String.valueOf(rect.getMinY()));
-		prop.setProperty(CAPTURE_DIM_RIGHT_BOTTOM_X, String.valueOf(rect.getMaxX()));
-		prop.setProperty(CAPTURE_DIM_RIGHT_BOTTOM_Y, String.valueOf(rect.getMaxY()));
-	}
+	// public Rectangle getCaptureRectangle() {
+	// int leftTopX = prop.getInt(CAPTURE_DIM_LEFT_TOP_X);
+	// int leftTopY = prop.getInt(CAPTURE_DIM_LEFT_TOP_Y);
+	// int rightBottomX = prop.getInt(CAPTURE_DIM_RIGHT_BOTTOM_X);
+	// int rightBottomY = prop.getInt(CAPTURE_DIM_RIGHT_BOTTOM_Y);
+	//
+	// return new Rectangle(leftTopX, leftTopY, rightBottomX - leftTopX,
+	// rightBottomY - leftTopY);
+	// }
+	//
+	// public void setCaptureRectangle(Rectangle rect) {
+	// prop.setProperty(CAPTURE_DIM_LEFT_TOP_X, String.valueOf(rect.getMinX()));
+	// prop.setProperty(CAPTURE_DIM_LEFT_TOP_Y, String.valueOf(rect.getMinY()));
+	// prop.setProperty(CAPTURE_DIM_RIGHT_BOTTOM_X,
+	// String.valueOf(rect.getMaxX()));
+	// prop.setProperty(CAPTURE_DIM_RIGHT_BOTTOM_Y,
+	// String.valueOf(rect.getMaxY()));
+	// }
 
 	public JiraConfig getJiraConfig() {
-		return new JiraConfig(prop.getString(JIRA_URL), prop.getString(JIRA_USERNAME));
+		return new JiraConfig(props.getString(JIRA_URL), props.getString(JIRA_USERNAME));
 	}
 
 	public void setJiraConfig(JiraConfig conf) {
-		prop.setProperty(JIRA_URL, conf.getUrl());
-		prop.setProperty(JIRA_USERNAME, conf.getUsername());
+		props.setProperty(JIRA_URL, conf.getUrl());
+		props.setProperty(JIRA_USERNAME, conf.getUsername());
 	}
 
 	public static class JiraConfig {
