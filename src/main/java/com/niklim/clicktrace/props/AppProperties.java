24d1253c71943d79fdedf267156c7ab183c89311
Marcin Nikliborc <marcin.nikliborc@gmail.com>
2014-02-26 09:31:26 +0100

    path to clicktrace.log in app.properties

--- src/main/java/com/niklim/clicktrace/props/AppProperties.java
+++ src/main/java/com/niklim/clicktrace/props/AppProperties.java
@@ -2,26 +2,50 @@ package com.niklim.clicktrace.props;
 
 import java.io.File;
 
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
- * App specific properties. Very unlikely to be modified by the user.
+ * App specific properties. Very unlikely to be modified by the user. For
+ * Windows installation it stores path to user.properties and log file.
  */
 @Singleton
 public class AppProperties extends AbstractProperties {
+	private static final String LOG4J_FILE_APPENDER_NAME = "FA";
+
 	private static final String APP_PROPERTIES_PATH = "app.properties";
 	private static final String JIRA_REST_CLICKTRACE_IMPORT_PATH = "jira.rest.clicktrace.import.path";
 	private static final String USER_PROPERTIES_PATH = "user.properties.path";
+	private static final String LOG_FILE_PATH = "log.file.path";
 
 	@Inject
 	protected void init() {
 		super.init();
+
+		initLog4j();
+	}
+
+	private void initLog4j() {
+		FileAppender fa = new FileAppender();
+		fa.setName(LOG4J_FILE_APPENDER_NAME);
+		fa.setFile(getLogPath());
+		fa.setLayout(new PatternLayout("%d{dd.MM.yyyy HH:mm:ss,SS} %-5p %C{1} - %m%n"));
+		fa.setThreshold(Level.DEBUG);
+		fa.setAppend(true);
+		fa.activateOptions();
+
+		Logger.getRootLogger().addAppender(fa);
 	}
 
 	static {
 		defaults.put(JIRA_REST_CLICKTRACE_IMPORT_PATH, "/rest/clicktrace/1.0/clicktrace/import");
 		defaults.put(USER_PROPERTIES_PATH, "user.properties");
+		defaults.put(LOG_FILE_PATH, "clicktrace.log");
 	}
 
 	@Override
@@ -35,5 +59,9 @@ public class AppProperties extends AbstractProperties {
 
 	public String getUserPropertiesPath() {
 		return props.getString(USER_PROPERTIES_PATH);
+	}
+
+	public String getLogPath() {
+		return props.getString(LOG_FILE_PATH);
 	}
 }
